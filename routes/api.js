7d3759fecd4217f8df45442f17cc94e545e4695d
Marlee McInelly <marleemcinelly@gmail.com>
2021-03-27 13:21:40 -0700

    changed db name in api.js to BudgetTrackerPWA

--- routes/api.js
+++ routes/api.js
@@ -3,31 +3,31 @@ const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(BudgetTrackerPWA => {
+      res.json(BudgetTrackerPWA);
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.status(404).json(err);
     });
 });
 
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(BudgetTrackerPWA => {
+      res.json(BudgetTrackerPWA);
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.status(404).json(err);
     });
 });
 
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(BudgetTrackerPWA => {
+      res.json(BudgetTrackerPWA);
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.status(404).json(err);
     });
 });
 

ad46411263823f2ccd0118a4d7204a93c8e27ee6
Marlee McInelly <marleemcinelly@gmail.com>
2021-03-27 14:00:51 -0700

    edits

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
